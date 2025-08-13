f916f48150ab1fc039b65ee033902698828fcf93
Saikumar1801 <tsaikumar1801@gmail.com>
2025-08-14 01:28:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // --- Firebase Configuration ---
 const firebaseConfig = {
-    apiKey: "AIzaSyCkXiKhWwx_W29ZFU8mmB-eecFHt1WZCX4",
-    authDomain: "chat-app-6194f.firebaseapp.com",
-    projectId: "chat-app-6194f",
-    storageBucket: "chat-app-6194f.appspot.com",
-    messagingSenderId: "432201991680",
-    appId: "1:432201991680:web:96ac04f905881f5332fae5",
-    measurementId: "G-5MG6QESZ5K"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
+
 
 // --- Firebase Initialization ---
 firebase.initializeApp(firebaseConfig);
@@ -1067,4 +1067,5 @@ async function deleteRoomForSelf() {
         await userRoomRef.delete();
         resetChatView();
     } catch(error) { if(error) console.error("Error leaving room:", error); }
+
 }
