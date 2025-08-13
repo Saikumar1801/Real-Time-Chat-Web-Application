16ff2b2f420f7c1476930ab4ce6513b3d440dacc
Saikumar1801 <tsaikumar1801@gmail.com>
2025-08-14 01:28:50 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCkXiKhWwx_W29ZFU8mmB-eecFHt1WZCX4",
-    authDomain: "chat-app-6194f.firebaseapp.com",
-    projectId: "chat-app-6194f",
-    storageBucket: "chat-app-6194f.appspot.com",
-    messagingSenderId: "432201991680",
-    appId: "1:432201991680:web:96ac04f905881f5332fae5",
-    measurementId: "G-5MG6QESZ5K"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -25,4 +24,5 @@ messaging.onBackgroundMessage((payload) => {
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
+
 });
